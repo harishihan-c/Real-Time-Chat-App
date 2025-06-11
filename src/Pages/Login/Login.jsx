054fafa913bb1eacdece4059faa7aa364ad69625
harishihan-c <harishihan@outlook.com>
2025-06-11 23:45:48 +0530

    feat(login): Add basic layout for the login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -3,8 +3,23 @@ import "./Login.css";
 
 const Login = () => {
     return(
-        <div>
-
+        <div className="login">
+            <img src='' alt='logo' />
+            <form className="login-form">
+                <h2>Sign Up</h2>
+                <input type="text" placeholder="Username" className="username-input"/>
+                <input type="email" placeholder="Email Address" className="email-input"/>
+                <input type="password" placeholder="Password" className="password-input"/>
+                <input type="password" placeholder="Confirm Password" className="confirm-password-input"/>
+                <button type="submit">Sign Up</button>
+                <div className="login-term">
+                    <input type="checkbox" />
+                    <p>I accept the terms and conditions</p>
+                </div>
+                <div className="login-forgot">
+                    <p>Already have an account <span>Sign in</span></p>
+                </div>
+            </form>
         </div>
     )
 }
