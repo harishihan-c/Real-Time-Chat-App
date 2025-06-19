6f68a5c047be49a2fe114da7c33cff5de94a2880
harishihan-c <harishihan@outlook.com>
2025-06-19 17:17:08 +0530

    add images

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,35 +1,38 @@
 import React from 'react';
 import "./Login.css";
+import assets from '../../assets/assets.js'
 
 const Login = () => {
     return(
         <div className="signup">
-            <div className="logo">
-                <img className="img-logo"/>
-                <h4>Vanga Kathaipam</h4>
-            </div>
+
             <div className="background">
-                <img className="img-background"/>
+                <div className="logo">
+                    <img className="img-logo" src={assets.logo}/>
+                    <h4>Vanga Kathaipam</h4>
+                </div>
+
+                <img className="img-background" src={assets.background}/>
             </div>
-            <div className="form-section">
-                <h2> Create an account</h2>
-                <div className="google">
-                    <img />
-                    <input placeholder="Signup with google" />
-                </div>
-                <div className="facebook">
-                    <img />
-                    <input placeholder="Signup with facebook" />
-                </div>
+           {/* <div className="form-section">*/}
+           {/*     <h2> Create an account</h2>*/}
+           {/*     <div className="google">*/}
+           {/*         <img />*/}
+           {/*         <input placeholder="Signup with google" />*/}
+           {/*     </div>*/}
+           {/*     <div className="facebook">*/}
+           {/*         <img />*/}
+           {/*         <input placeholder="Signup with facebook" />*/}
+           {/*     </div>*/}
 
-                <input placeholder="Email Address" />
-                <input placeholder="Password" />
-                <input type="radio" id="correct" />
-                <label for="correct" > I agree to term and conditions </label>
-                <button className="button"> Signup</button>
-                <p>Already have an account log in</p>
+           {/*     <input placeholder="Email Address" />*/}
+           {/*     <input placeholder="Password" />*/}
+           {/*     <input type="radio" id="correct" />*/}
+           {/*     <label for="correct" > I agree to term and conditions </label>*/}
+           {/*     <button className="button"> Signup</button>*/}
+           {/*     <p>Already have an account log in</p>*/}
 
-           </div>
+           {/*</div>*/}
         </div>
     )
 }
