d9c4be5aef253a072584815aec88a5839ea60e7f
harishihan-c <harishihan@outlook.com>
2025-06-19 15:51:12 +0530

    Add signup layout

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -3,23 +3,33 @@ import "./Login.css";
 
 const Login = () => {
     return(
-        <div className="login">
-            <img src='' alt='logo' />
-            <form className="login-form">
-                <h2>Sign Up</h2>
-                <input type="text" placeholder="Username" className="username-input"/>
-                <input type="email" placeholder="Email Address" className="email-input"/>
-                <input type="password" placeholder="Password" className="password-input"/>
-                <input type="password" placeholder="Confirm Password" className="confirm-password-input"/>
-                <button type="submit">Sign Up</button>
-                <div className="login-term">
-                    <input type="checkbox" />
-                    <p>I accept the terms and conditions</p>
+        <div className="signup">
+            <div className="logo">
+                <img className="img-logo"/>
+                <h4>Vanga Kathaipam</h4>
+            </div>
+            <div className="background">
+                <img className="img-background"/>
+            </div>
+            <div className="form-section">
+                <h2> Create an account</h2>
+                <div className="google">
+                    <img />
+                    <input placeholder="Signup with google" />
                 </div>
-                <div className="login-forgot">
-                    <p>Already have an account <span>Sign in</span></p>
+                <div className="facebook">
+                    <img />
+                    <input placeholder="Signup with facebook" />
                 </div>
-            </form>
+
+                <input placeholder="Email Address" />
+                <input placeholder="Password" />
+                <input type="radio" id="correct" />
+                <label for="correct" > I agree to term and conditions </label>
+                <button className="button"> Signup</button>
+                <p>Already have an account log in</p>
+
+           </div>
         </div>
     )
 }
