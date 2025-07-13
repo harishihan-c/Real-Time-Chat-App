035f8327d921e22ee80c3215a4f6f22eaf7cb594
harishihan-c <harishihan@outlook.com>
2025-07-14 00:47:29 +0530

    Add: structure for top and bottom bar

--- src/Components/ChatBox/ChatBox.jsx
+++ src/Components/ChatBox/ChatBox.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import "./ChatBox.css"
+import assets from "../../assets/assets.js";
 
 const ChatBox = () => {
 
+    return(
+        <div className="chatbox">
+            <div className="chatbox-top">
+                <img src={assets.contact1} />
+                <p> Harry <span>online</span></p>
+            </div>
+
+            <div className="chatbox-bottom">
+                <input type="text" placeholder="Type a message" />
+                <input type="file" />
+            </div>
+        </div>
+    )
 }
 
 export default ChatBox
