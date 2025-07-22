8d62673817ebd6408146dafc08bc2d1ec994f9e2
harishihan-c <harishihan@outlook.com>
2025-07-22 16:07:19 +0530

    ft: profile page completed

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import "./Profile.css";
+import assets from "../../assets/assets.js";
 
 const Profile = () => {
     return(
-        <div>
+        <div className="profile">
+            <div className="profile-container">
+                <img className="close" src={assets.close} alt=""/>
+                <form>
+                    <h3>Profile Settings</h3>
+                    <label htmlFor="avatar">
+                        <input type="file" id="avatar" accept='.png, .jpg, .jpeg' hidden />
+                        <img src={assets.contact1} alt=""/>
+                        change profile photo
+                    </label>
+                    <input type="text" placeholder="your Name" required />
+                    <button type="submit" >Save</button>
+                </form>
+            </div>
 
         </div>
     )
