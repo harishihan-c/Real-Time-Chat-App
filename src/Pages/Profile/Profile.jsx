6646257a94cfcacbc437f724ba4917a9112ae78b
harishihan-c <harishihan@outlook.com>
2025-07-23 13:26:00 +0530

    add: profile image add option working

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Profile.css";
 import assets from "../../assets/assets.js";
 
 const Profile = () => {
+
+    const [image, setImage] = useState(false);
+
     return(
         <div className="profile">
             <div className="profile-container">
@@ -10,8 +13,8 @@ const Profile = () => {
                 <form>
                     <h3>Profile Settings</h3>
                     <label htmlFor="avatar">
-                        <input type="file" id="avatar" accept='.png, .jpg, .jpeg' hidden />
-                        <img src={assets.contact1} alt=""/>
+                        <input type="file" onChange={(e) => {setImage(e.target.files[0])}} id="avatar" accept='.png, .jpg, .jpeg' hidden />
+                        <img src={image? URL.createObjectURL(image): assets.profileUser} alt=""/>
                         change profile photo
                     </label>
                     <input type="text" placeholder="your Name" required />
